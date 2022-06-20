cca10717768b1fc2cf6d756d0778a09255529dd2
konstantin <kostel1995@gmail.com>
2022-06-20 17:39:04 +0300

    master

--- lessson12/task3/index.js
+++ lessson12/task3/index.js
@@ -1,12 +1,15 @@
+const regex = /[A-Za-z]/g;
+const isString = (str) => str.toString().match(regex);
 const cleanTransactionsList = (transactions) => {
-  transactions.filter((elem) => !isNaN(elem));
-  return transactions.map(
-    (elem) =>
-      "$" +
-      (typeof elem !== "number"
-        ? parseFloat(elem.replace(/[^\d\.]*/g, "")).toFixed(2)
-        : elem.toFixed(2))
-  );
+  return transactions
+    .filter((e) => !isString(e))
+    .map(
+      (elem) =>
+        "$" +
+        (typeof elem !== "number"
+          ? parseFloat(elem.replace(/[^\d\.]*/g, "")).toFixed(2)
+          : elem.toFixed(2))
+    );
 };
 
-console.log(cleanTransactionsList([" 1.9 ", "16.4", 17, " 1 dollar "]));
+console.log(cleanTransactionsList(["  1.9 ", "16.4", 17, " 1 dollar "]));
